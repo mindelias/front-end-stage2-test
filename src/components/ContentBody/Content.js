256d7066169cf6e4dd45d1249491c1ee4ee51f9f
mindelias@gmail.com <mindelias@gmail.com>
2020-03-20 11:38:54 +0100

    add navbar and side bar

---
+++ src/components/ContentBody/Content.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Cards from "../Cards/Cards";
+import icon3 from "../../common/imgs/icon3.png";
+import ring1 from "../../common/imgs/Capture.PNG";
+import IconG from "../../common/imgs/grn-money.png";
+import ICONR from "../../common/imgs/red-money.png";
+
+function Content() {
+  return (
+    <div className="row mt-5 right">
+      <div className="col-7">
+        <Cards
+          img={icon3}
+          text="Total Annula capel Budget"
+          text2="Annual"
+          img2={ring1}
+          iconG={IconG}
+          iconR={ICONR}
+        />
+        <Cards
+          img={icon3}
+          text="Total Annula capel Budget"
+          text2="Annual"
+          img2={ring1}
+          iconG={IconG}
+          iconR={ICONR}
+        />
+      </div>
+      <div className="col-4 left">
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;

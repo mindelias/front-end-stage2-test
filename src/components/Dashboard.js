6ed42c752fbc96a5444981d42541fc69ecb17325
mindelias@gmail.com <mindelias@gmail.com>
2020-07-13 11:54:52 +0100

    changes font-size

--- src/components/Dashboard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Container } from "reactstrap";
-function Dashboard() {
-  return <Container className="container" fluid={true}></Container>;
-}
-
-export default Dashboard;
